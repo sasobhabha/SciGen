d921d6a129247f929019dff0d25741f575968cba
shashwath <shashwath.manjunath@gmail.com>
2025-12-10 16:36:06 -0800

    Initial Commit

---
+++ SciGen/SciGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SciGenApp.swift
+//  SciGen
+//
+//  Created by Shashwath Manjunath on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SciGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SciGenTests/SciGenTests.swift
@@ -0,0 +1,17 @@
+//
+//  SciGenTests.swift
+//  SciGenTests
+//
+//  Created by Shashwath Manjunath on 12/10/25.
+//
+
+import Testing
+@testable import SciGen
+
+struct SciGenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
